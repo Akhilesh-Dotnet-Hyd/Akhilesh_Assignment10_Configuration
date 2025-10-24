0d4a983836eaa1504ba03c873a0e1a72c1ad4d77
Somashila Akhilesh <Somashila.Akhilesh@encora.com>
2025-10-24 17:34:44 +0530

    Add project files.

---
+++ Assignment9_Logging/Controllers/ConfigController.cs
@@ -0,0 +1,39 @@
+﻿using Assignment9_Logging.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Assignment9_Logging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MySettings _mySettings;
+        public ConfigController(IConfiguration configuration, IOptions<MySettings> mySettings)
+        {
+            _configuration = configuration;
+            _mySettings = mySettings.Value;
+        }
+        [HttpGet("appsettings")]
+        public IActionResult GetAppSettings()
+        {
+            var manual = new
+            {
+                Source = "Manual",
+                ApplicationName = _configuration["MySettings:ApplicationName"],
+                MaxItems = _configuration["MySettings:MaxItems"],
+                Version = _configuration["MySettings:Version"]
+            };
+            var options = new
+            {
+                Source = "IOptions",
+                ApplicationName = _mySettings.ApplicationName,
+                MaxItems = _mySettings.MaxItems,
+                Version = _mySettings.Version
+            };
+            return Ok(new { manual, options });
+        }
+    }
+}
---
+++ Assignment9_Logging/Models/MySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment9_Logging.Models
+{
+    public class MySettings
+    {
+        public string ApplicationName { get; set; }
+        public int MaxItems { get; set; }
+        public string Version { get; set; }
+    }
+}
